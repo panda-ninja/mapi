b1414b9bcc718748be07ef70e0a22eb31a32b8ec
josue <josue_luistj@hotmail.com>
2016-12-13 17:55:50 -0500

    pagina principal y footer

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>gotoperu</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="fondo">
+<div class="container encima">
+	
+		<div class="masthead">
+	        <nav>
+	          <ul class="nav nav-justified me">
+	            <li class="chat centrado">
+	            <div class="row">
+	            	<div class="col-md-4">
+	            		<img src="img/fono.png" alt="" class=" img-responsive">
+	            	</div>
+	            	<div class="col-md-8 margin-15">
+	            		chatea con
+	            		<br>
+	            		nosotros
+	            	</div>
+	            </div>
+	            </li>
+	            <li class="llamanos centrado">
+	            <div class="row">
+	            	<div class="col-md-4">
+	            		<img src="img/fono.png" alt="" class=" img-responsive">
+	            	</div>
+	            	<div class="col-md-8 margin-15">
+	            		llamanos
+	            		<br>
+	            		999 888 777
+	            		<br>
+	            		999 888 777
+	            	</div>
+	            </div>
+	            </li>
+	            <li class="llamanos centrado">
+	            <div class="row">
+	            	<div class="col-md-12 margin-15">
+	            		reserve las 24 horas
+	            		<br>
+	            		info@gotoperu.com
+	            		<br>
+	            		info@gotoperu.com
+	            	</div>
+	            </div>
+	            </li>
+	            <li  class="centrado">
+	 	            <div class="row">
+		            	<div class="col-md-12">
+		            		<strong class="agrandar">nosotros te llamamos</strong>
+		            	</div>
+		            </div>
+	            </li>
+	            <li class="centrado">
+	            	<div class="row">
+	            		<div class="col-md-4">
+	            			<img src="img/brazil.png" alt="" class="img-responsive">
+	            			<img src="img/eeuu.png" alt="" class="img-responsive">
+	            		</div>
+		            	<div class="col-md-8 margin-15">
+		            		
+		            		
+		            		<strong class="agrandar">english</strong>
+		            		<br>
+		            		<strong class="agrandar">Brasil</strong>
+		            	</div>
+	            	</div>
+	            </li>
+	            
+	          </ul>
+	        </nav>
+      </div>
+      <div class="main">
+      	<header id="header" role="banner" class="">
+
+      		<img src="img/tripadvisor.png" alt="" class="img-responsive imagen">
+      		<p class="mail">reserve ahora: <strong>info@gotoperu.com</strong></p>
+      		
+      	</header>
+      	<div>
+      		<h3>tours a machupicchu </h3>
+      	</div>
+      	<div class="container ">
+      		<div class="col-md-7 col-sm-12 full">
+      			<section >
+      				<h5 class="subtitulos">tours de 1 a 30 dias, incluye transporte, alimentos y más</h5>
+      				<ul id="listas" class="full">
+      					<li>
+      						<a href="#">Tours a machupicchu en un dia:valle  sagrado, pisaq, qenqoro</a>
+      					</li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae!</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, esse.</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque natus ex aliquam.</a></li>
+
+      				</ul>
+      			</section>
+      			<section >
+      				<h5 class="subtitulos">Tours en todo el peru</h5>
+      				<ul id="listas" class="full">
+      					<li>
+      						<a href="#">Tours a machupicchu en un dia</a>
+      					</li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae!</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, esse.</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque natus ex aliquam.</a></li>
+
+      				</ul>
+      			</section>
+      			<section >
+      				<h5 class="subtitulos">Tours en Sudamerica</h5>
+      				<ul id="listas" class="full">
+      					<li>
+      						<a href="#">Tours a machupicchu en un dia</a>
+      					</li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae!</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, esse.</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque natus ex aliquam.</a></li>
+
+      				</ul>
+      			</section>
+      			<section >
+      				<h5 class="subtitulos">Tours en Cusco</h5>
+      				<ul id="listas" class="full">
+      					<li>
+      						<a href="#">Tours a machupicchu en un dia</a>
+      					</li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae!</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, esse.</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque natus ex aliquam.</a></li>
+ 
+      				</ul>
+      			</section>
+      			<section >
+      				<h5 class="subtitulos">Trekking a  Machupicchu</h5>
+      				<ul id="listas" class="full">
+      					<li>
+      						<a href="#">Tours a machupicchu en un dia</a>
+      					</li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae!</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, esse.</a></li>
+      					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque natus ex aliquam.</a></li>
+
+      				</ul>
+      			</section>
+      		</div>
+      		<div class="col-md-5 col-sm-12 ">
+      			<section>
+      				<h5 class="subextra"> nuestra empresa es reconocida a nivel nacional</h5>
+      				<div class="orden">
+      					<iframe width="100%" height="248" src="https://www.youtube.com/embed/Chq4BnQNUkw" frameborder="0" allowfullscreen></iframe>
+      				</div>
+      			</section>
+      			<section>
+      				<h5 class="subextra"> mire los testimonios</h5>
+      				<div class="orden">
+      					<iframe width="100%" height="248" src="https://www.youtube.com/embed/Chq4BnQNUkw" frameborder="0" allowfullscreen></iframe>
+      				</div>
+      				<div class="orden">
+      					<iframe width="100%" height="248" src="https://www.youtube.com/embed/Chq4BnQNUkw" frameborder="0" allowfullscreen></iframe>
+      				</div>
+      			</section>
+      			<section>
+      				<h5 class="subextra"> opiniones de nuestros pasajeros</h5>
+      				<div class="orden">
+      					<p class="comentarios">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, maiores praesentium architecto officia odit atque minima ipsam eaque tempora. Perferendis!
+      					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero officia delectus culpa, enim incidunt alias ratione veniam quibusdam. Porro, necessitatibus!</p>
+      					<p>
+      						<strong class="autores">nombre:</strong>josue luis mancilla
+      						<br>
+      						<strong class="autores">pais:</strong> peru <br>
+      						<strong class="autores">fecha:</strong> 15/15/15
+      					</p>
+      				</div>
+      				<div class="orden">
+      					<p class="comentarios">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, maiores praesentium architecto officia odit atque minima ipsam eaque tempora. Perferendis!
+      					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero officia delectus culpa, enim incidunt alias ratione veniam quibusdam. Porro, necessitatibus!</p>
+      					<p>
+      						<strong class="autores">nombre:</strong>josue luis mancilla
+      						<br>
+      						<strong class="autores">pais:</strong> peru <br>
+      						<strong class="autores">fecha:</strong> 15/15/15
+      					</p>
+      				</div>
+      			</section>
+      		</div>
+      	</div>
+      </div>
+	
+</div>
+</body>
+<footer>
+	<div class="container">
+		<div class="col-md-4">
+			<h5>GOTOPERU</h5>
+			<hr>
+			<p><strong>E-mail:</strong> info@gotoperu.com <br> <strong>Nuestros Telefonos:</strong>999999999 - 99999999</p>
+		</div>
+		<div class="col-md-5">
+			<h5 class="centro">MACHUPICCHU LO ESPERA</h5>
+			<hr>
+			<div class="row">
+				<div class="col-md-6">
+					<ul>
+						<li><a href="#">Destinos a Peru</a></li>
+						<li><a href="#">Aerolineas a Peru</a></li>
+						<li><a href="#">Hoteles en Peru</a></li>
+						
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<ul>
+						<li><a href="#">Entradas a Machupicchu</a></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<h5>SIGUENOS</h5>
+			<hr>
+			<ul>
+				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts">facebook</a></li>
+				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts">twiter</a></li>
+				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts">instagram</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+</html>
