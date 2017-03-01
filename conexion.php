3b4c7702421360b3b68bc076f11fb2b37034c4b4
josue <josue_luistj@hotmail.com>
2017-03-01 17:04:16 -0500

    cambio de conexion de local a remoto

--- conexion.php
+++ conexion.php
@@ -2,12 +2,12 @@
 
 	function conectar(){
 		$host = "localhost";
-		$user = "root";
+		/*$user = "root";
 		$pas = "";
-		$db = "vivaperu";
-		/*$user = "b2u4b3e3_company";
+		$db = "vivaperu";*/
+		$user = "b2u4b3e3_company";
 		$pas = "mcompany_2017";
-		$db = "b2u4b3e3_mcompany";*/
+		$db = "b2u4b3e3_mcompany";
 		$port = 3306;
 		$conexion= new mysqli($host, $user, $pas, $db);
 		mysqli_set_charset($conexion,'utf8');
