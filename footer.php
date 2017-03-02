801155165c2979fbb527a552186dc052cb540577
josue <josue_luistj@hotmail.com>
2017-03-02 12:19:43 -0500

    cambios en el footer

--- footer.php
+++ footer.php
@@ -1,28 +1,41 @@
 <footer>
 	<div class="container margin-top-footer">
+	<div class="row">
 		<div class="col-md-3">
-
+			
 		</div>
 		<div class="col-md-3">
-			<h5 class="footer-titulo">GOTOPERU</h5>
-			<hr>
-			<p class="letra-blanca"><strong class="letra-negra">E-mail:</strong> info@gotoperu.com <br> <strong class="letra-negra">Nuestros Telefonos:</strong>999999999 - 99999999</p>
+			<a href="index.php " class="footer-link"><h5>inicio</h5></a>
 		</div>
 		<div class="col-md-3">
-			<h5 class="centro footer-titulo">MACHUPICCHU LO ESPERA</h5>
-			<hr>
-			<li><a href="destinos.php">Destinos a Peru</a></li>
-
-
+			<a href="destinos.php" class="footer-link"><h5>Destinos</h5></a>
 		</div>
 		<div class="col-md-3">
-			<h5 class="footer-titulo">SIGUENOS</h5>
-			<hr>
+			<a href="testimonios.php" class="footer-link"><h5>Testimonios</h5></a>
+		</div>
+	</div>
+	<div class="row espacio-10px" >
+		<div class="col-md-2">
+
+		</div>
+		<div class="col-md-4">
+			<h5 class="footer-titulo">CONTACTESE CON NOSOTROS:</h5>
+			<p class="letra-blanca"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@gotoperu.com <br> <i class="fa fa-volume-control-phone" aria-hidden="true"></i>99999999</p>
+		</div>
+
+		<div class="col-md-4">
+			<h5 class="footer-titulo">SIGUENOS:</h5>
 			<ul class="listas-blancas">
 				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i>facebook</a></li>
 				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts"><i class="fa fa-twitter" aria-hidden="true"></i>twiter</a></li>
 				<li><a href="https://www.facebook.com/GOTOPERUcom/?fref=ts"><i class="fa fa-instagram" aria-hidden="true"></i>instagram</a></li>
 			</ul>
 		</div>
+		<div class="col-md-2">
+				<img src="img/logo.png" alt="" class="img-responsive largo">
+		</div>
+	</div>
+		
+		
 	</div>
 </footer>
